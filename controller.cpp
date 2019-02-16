8b66b9bcf4c2f0906712dc159f673e5f560b7e7f
Mikołaj Werner <m.werner9205@gmail.com>
2019-02-16 19:44:53 +0100

    - implemented methods for sending data between qml views

--- controller.cpp
+++ controller.cpp
@@ -1,6 +1,19 @@
 #include "controller.h"
 
-controller::controller(QObject *parent) : QObject(parent)
+Controller::Controller(QObject *parent) : QObject(parent)
 {
 
+}
+
+
+void Controller::passQMLFile(QString fileName){
+    emit passedQMLFileName(fileName);
+}
+
+void Controller::passDataToChartView(int moduleId, QString startDate, QString endDate, int measurementType){
+    emit passedDataToChartView(moduleId, startDate, endDate, measurementType);
+}
+
+void Controller::passModuleId(int moduleId, QString moduleName){
+    emit passedModuleId(moduleId, moduleName);
 }
--- controller.h
+++ controller.h
@@ -2,16 +2,25 @@
 #define CONTROLLER_H
 
 #include <QObject>
+#include "controlnetapi.h"
 
-class controller : public QObject
+class Controller : public QObject
 {
     Q_OBJECT
 public:
-    explicit controller(QObject *parent = nullptr);
+    explicit Controller(QObject *parent = nullptr);
+
 
 signals:
+    void passedQMLFileName(QString fileName);
+    void passedDataToChartView(int moduleId, QString startDate, QString endDate, int measurementType);
+    void passedModuleId(int moduleId, QString moduleName);
 
 public slots:
+    void passQMLFile(QString fileName);
+    void passDataToChartView(int moduleId, QString startDate, QString endDate,  int measurementType);
+    void passModuleId(int moduleId, QString moduleName);
+
 };
 
 #endif // CONTROLLER_H
